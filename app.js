8c116b25a52012c98ffbe47919f761ea972e1336
eutopi <tongyuzhou98@gmail.com>
2020-10-27 19:26:54 -0400

    displayed fetched plants to frontend

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=bHra1j9vmn0WazgawJMdRojCX2YzRbA5BRP5EqUA1Y8";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -24,8 +24,22 @@ const corsPromise = () =>
 corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
+      parseData(request.response)
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
     })
 );
 
-//// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+const parseData = (response) => {
+  var plantDict = JSON.parse(response)
+  var commonPlantNames = plantDict.data.map(x => x.common_name)
+  console.log(plantDict)
+  addPrettyPlantList(commonPlantNames)
+}
+
+const addPrettyPlantList = (plants) => {
+  console.log(plants)
+  document.getElementById('plants').innerHTML = ""
+  for (var i in plants) {
+    document.getElementById('plants').innerHTML += plants[i] + "<br>"
+  }
+}
